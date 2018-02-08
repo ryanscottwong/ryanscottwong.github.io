af8f7d2e5d4f90ebbd0dbc48d3000d8217aecec1
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-07 16:30:22 -0800

    Update hw3-2.js

--- cnit133/js/hw3-2.js
+++ cnit133/js/hw3-2.js
@@ -14,16 +14,16 @@ $(document).ready(function(){
   $("#myform").validate({
     // Rules for each input item
     rules: {
-      hwAvg: { required: true, number: true, min: 0, max: 100},
-      midExam: { required: true, number: true, min: 0, max: 100},
-      finalExam: { required: true, number: true, min: 0, max: 100},
-      acr: { required: true, number: true, min: 0, max: 100}
+      item1: {required: true, number: true, min: 0},
+      item2: {required: true, number: true, min: 0},
+      item3: {required: true, number: true, min: 0},
+      item4: {required: true, number: true, min: 0}
     },
     messages: {
-       hwAvg: "Please enter a number between 0 and 100",
-       midExam: "Please enter a number between 0 and 100",
-       finalExam: "Please enter a number between 0 and 100",
-       acr: "Please enter a number between 0 and 100"
+       item1: "Please enter a number",
+       item2: "Please enter a number",
+       item3: "Please enter a number",
+       item4: "Please enter a number"
     },
   errorPlacement: function(error, element) {
     if ( element.is(":text")) {
@@ -39,12 +39,12 @@ $(document).ready(function(){
 function process() {
 if ($("#myform").valid()) {
 // define variables
-var hwAvgb, midExamb, finalExamb, acrb, finalAvg, grade;
+var item1b, item2b, item3b, item4, finalAvg, grade;
 // get input number value from form using getElementById
- hwAvgb = document.getElementById("hwAvg").value;
- midExamb = document.getElementById("midExam").value;
- finalExamb = document.getElementById("finalExam").value;
- acrb = document.getElementById("acr").value;
+ item1b = document.getElementById("item1").value;
+ item2b = document.getElementById("item2").value;
+ item3b = document.getElementById("item3").value;
+ item4b = document.getElementById("item4").value;
  // calculate final average
  finalAvg = parseInt((.5 * hwAvgb) + (.2 * midExamb) + (.2 * finalExamb) + (.1 * acrb));
  // determine grade based on final average

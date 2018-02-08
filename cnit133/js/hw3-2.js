ace03802e2f56056ab677b754588a5b1647f03a8
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-08 11:08:16 -0800

    Update hw3-2.js

--- cnit133/js/hw3-2.js
+++ cnit133/js/hw3-2.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
 
+  // This script shows/hides instructions.
+  $("#button").click(function(){
+    $("#panel").slideToggle("slow");
+  }); // end show/hide
+
   /* JQuery for validation displays error message if form input is not a number */
-  $("#my_form").validate({
+  $("#myform").validate({
     // Rules for each input item
     rules: {
-      item1: {required: true, number: true, min: 0, max: 100},
-      item2: {required: true, number: true, min: 0, max: 100},
-      item3: {required: true, number: true, min: 0, max: 100},
-      item4: {required: true, number: true, min: 0, max: 100}
+      hwAvg: { required: true, number: true, min: 0, max: 100},
+      midExam: { required: true, number: true, min: 0, max: 100},
+      finalExam: { required: true, number: true, min: 0, max: 100},
+      acr: { required: true, number: true, min: 0, max: 100}
     },
     messages: {
-       item1: "Please enter a number",
-       item2: "Please enter a number",
-       item3: "Please enter a number",
-       item4: "Please enter a number"
+       hwAvg: "Please enter a number between 0 and 100",
+       midExam: "Please enter a number between 0 and 100",
+       finalExam: "Please enter a number between 0 and 100",
+       acr: "Please enter a number between 0 and 100"
     },
   errorPlacement: function(error, element) {
     if ( element.is(":text")) {
@@ -22,17 +27,46 @@ $(document).ready(function(){
     else {
       error.insertAfter(element);
     }
-  });	// end validation
+  }
+});	// end validation
 }); // document ready
 
 function process() {
-  if ($("#my_form").valid()) {
-    // define variables
-    var item1b, item2b, item3b, item4b;
-    // get input number value from form using getElementById
-    item1b = document.getElementById("item1").value;
-    item2b = document.getElementById("item2").value;
-    item3b = document.getElementById("item3").value;
-    item4b = document.getElementById("item4").value;
-  }
+if ($("#myform").valid()) {
+// define variables
+var hwAvgb, midExamb, finalExamb, acrb, finalAvg, grade;
+// get input number value from form using getElementById
+ hwAvgb = document.getElementById("hwAvg").value;
+ midExamb = document.getElementById("midExam").value;
+ finalExamb = document.getElementById("finalExam").value;
+ acrb = document.getElementById("acr").value;
+ // calculate final average
+ finalAvg = parseInt((.5 * hwAvgb) + (.2 * midExamb) + (.2 * finalExamb) + (.1 * acrb));
+ // determine grade based on final average
+ if (finalAvg >= 90) {
+   grade = "A";
+ }
+ else if (finalAvg >= 80) {
+   grade = "B";
+ }
+ else if (finalAvg >= 70) {
+   grade = "C";
+ }
+ else if (finalAvg >= 60) {
+   grade = "D";
+ }
+ else {
+   grade = "F"
+ }
+ // display for grade F
+ if (grade == "F") {
+ document.forms["myform"].elements["comment"].value = ("Final average is " + finalAvg
+ + "\nFinal grade is " + grade + "\nStudent must retake the course");
+ }
+ // display for grades A-D
+ else {
+ document.forms["myform"].elements["comment"].value = ("Final average is " + finalAvg
+ + "\nFinal grade is " + grade);
+ }
+}
 }

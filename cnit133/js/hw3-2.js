937d813aa3068f6a23459584e2623613d4f59399
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 11:39:49 -0800

    Update hw3-2.js

--- cnit133/js/hw3-2.js
+++ cnit133/js/hw3-2.js
@@ -1,72 +1,22 @@
-$(document).ready(function(){
-
-  // This script shows/hides instructions.
-  $("#button").click(function(){
-    $("#panel").slideToggle("slow");
-  }); // end show/hide
-
-  /* JQuery for validation displays error message if form input is not a number */
-  $("#myform").validate({
-    // Rules for each input item
-    rules: {
-      item1sold: { required: true, number: true, min: 0, max: 100},
-      item2sold: { required: true, number: true, min: 0, max: 100},
-      item3sold: { required: true, number: true, min: 0, max: 100},
-      item4sold: { required: true, number: true, min: 0, max: 100}
-    },
-    messages: {
-       item1sold: "Please enter a number between 0 and 100",
-       item2sold: "Please enter a number between 0 and 100",
-       item3sold: "Please enter a number between 0 and 100",
-       item4sold: "Please enter a number between 0 and 100"
-    },
-  errorPlacement: function(error, element) {
-    if ( element.is(":text")) {
-      error.appendTo( element.parent());
-    }
-    else {
-      error.insertAfter(element);
-    }
-  }
-});	// end validation
-}); // document ready
-
-function process() {
-if ($("#myform").valid()) {
-// define variables
-var item1soldb, item2soldb, item3soldb, item4soldb, finalAvg, grade;
-// get input number value from form using getElementById
- item1soldb = document.getElementById("item1sold").value;
- item2soldb = document.getElementById("item2sold").value;
- item3soldb = document.getElementById("item3sold").value;
- item4soldb = document.getElementById("item4sold").value;
- // calculate final average
- finalAvg = parseInt((.5 * item1soldb) + (.2 * item2soldb) + (.2 * item3soldb) + (.1 * item4soldb));
- // determine grade based on final average
- if (finalAvg >= 90) {
-   grade = "A";
- }
- else if (finalAvg >= 80) {
-   grade = "B";
- }
- else if (finalAvg >= 70) {
-   grade = "C";
- }
- else if (finalAvg >= 60) {
-   grade = "D";
- }
- else {
-   grade = "F"
- }
- // display for grade F
- if (grade == "F") {
- document.forms["myform"].elements["comment"].value = ("Final average is " + finalAvg
- + "\nFinal grade is " + grade + "\nStudent must retake the course");
- }
- // display for grades A-D
- else {
- document.forms["myform"].elements["comment"].value = ("Final average is " + finalAvg
- + "\nFinal grade is " + grade);
- }
-}
+/* This script calculates how much the salesperson sold, how much they earned, and then outputs the results */
+function total()
+{
+  salaryWeekly = 200.00;
+  commissionRate = 0.09;
+  item1Sold = document.getElementById("item1Sold").value;
+  item2Sold = document.getElementById("item2Sold").value;
+  item3Sold = document.getElementById("item3Sold").value;
+  item4Sold = document.getElementById("item4Sold").value;
+  item1Price = 239.99;
+  item2Price = 129.75;
+  item3Price = 99.95;
+  item4Price = 350.89;
+  item1Total = (salaryWeekly + (commissionRate * item1Sold * item1Price)).toFixed(2);
+  item2Total = (salaryWeekly + (commissionRate * item2Sold * item2Price)).toFixed(2);
+  item2Total = (salaryWeekly + (commissionRate * item3Sold * item3Price)).toFixed(2);
+  item2Total = (salaryWeekly + (commissionRate * item4Sold * item4Price)).toFixed(2);
+  document.getElementById("item1Total").value = (gbp_value);
+  document.getElementById("item2Total").value = (can_value);
+  document.getElementById("item3Total").value = (eur_value);
+  document.getElementById("item4Total").value = (jpn_value);
 }

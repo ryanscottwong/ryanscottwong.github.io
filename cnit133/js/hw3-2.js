aae508c00f62432ab66b325351506b4d27e81f2e
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 12:17:10 -0800

    Update hw3-2.js

--- cnit133/js/hw3-2.js
+++ cnit133/js/hw3-2.js
@@ -3,18 +3,20 @@ function total()
 {
   salaryWeekly = 200.00;
   commissionRate = 0.09;
-  item1SoldB = document.getElementById("item1Sold").value;
-  item2SoldB = document.getElementById("item2Sold").value;
-  item3SoldB = document.getElementById("item3Sold").value;
-  item4SoldB = document.getElementById("item4Sold").value;
   item1Price = 239.99;
   item2Price = 129.75;
   item3Price = 99.95;
   item4Price = 350.89;
-  item1Total = (salaryWeekly + (commissionRate * item1SoldB * item1Price)).toFixed(2);
-  item2Total = (salaryWeekly + (commissionRate * item2SoldB * item2Price)).toFixed(2);
-  item3Total = (salaryWeekly + (commissionRate * item3SoldB * item3Price)).toFixed(2);
-  item4Total = (salaryWeekly + (commissionRate * item4SoldB * item4Price)).toFixed(2);
+  item1SoldB = document.getElementById("item1Sold").value;
+  item2SoldB = document.getElementById("item2Sold").value;
+  item3SoldB = document.getElementById("item3Sold").value;
+  item4SoldB = document.getElementById("item4Sold").value;
+  item1Total = (item1SoldB * item1Price).toFixed(2);
+  item2Total = (item2SoldB * item2Price).toFixed(2);
+  item3Total = (item3SoldB * item3Price).toFixed(2);
+  item4Total = (item4SoldB * item4Price).toFixed(2);
+  amountSold = item1Total + item2Total + item3Total + item4Total;
+  weeklyEarnings = salaryWeekly + (commissionRate * amountSold);
   document.getElementById("item1SoldB").value = (item1SoldB);
   document.getElementById("item2SoldB").value = (item2SoldB);
   document.getElementById("item3SoldB").value = (item3SoldB);
@@ -23,4 +25,6 @@ function total()
   document.getElementById("item2TotalB").value = (item2Total);
   document.getElementById("item3TotalB").value = (item3Total);
   document.getElementById("item4TotalB").value = (item4Total);
+  document.getElementById("amountSoldB").value = (amountSold);
+  document.getElementById("weeklyEarningsB").value = (weeklyEarnings);
 }

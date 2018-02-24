3b1af81f52bf17218c876ba4d4b5397c1994316e
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 15:16:28 -0800

    Update hw3-3.js

--- cnit133/js/hw3-3.js
+++ cnit133/js/hw3-3.js
@@ -28,7 +28,7 @@ function celsius() {
     // get input number value from form using getElementById
     tempF = document.getElementById("temp").value;
     // convert temperature to Fahrenheit
-    tempC = ((5 / 9) * (tempF - 32)).toFixed(0);
+    tempC = parseInt((5 / 9) * (tempF - 32), 10);
     // display Fahrenheit temperature
     document.forms["myform"].elements["conversion"].value = ("Temperature in Celsius is " + tempC);
   }

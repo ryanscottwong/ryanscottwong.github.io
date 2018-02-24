93c0c9069a7e3581d555264f2b9c475766a63c3f
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 11:49:28 -0800

    Update hw3-2.js

--- cnit133/js/hw3-2.js
+++ cnit133/js/hw3-2.js
@@ -3,20 +3,20 @@ function total()
 {
   salaryWeekly = 200.00;
   commissionRate = 0.09;
-  item1Sold = document.getElementById("item1Sold").value;
-  item2Sold = document.getElementById("item2Sold").value;
-  item3Sold = document.getElementById("item3Sold").value;
-  item4Sold = document.getElementById("item4Sold").value;
+  item1SoldB = document.getElementById("item1Sold").value;
+  item2SoldB = document.getElementById("item2Sold").value;
+  item3SoldB = document.getElementById("item3Sold").value;
+  item4SoldB = document.getElementById("item4Sold").value;
   item1Price = 239.99;
   item2Price = 129.75;
   item3Price = 99.95;
   item4Price = 350.89;
-  item1Total = (salaryWeekly + (commissionRate * item1Sold * item1Price)).toFixed(2);
-  item2Total = (salaryWeekly + (commissionRate * item2Sold * item2Price)).toFixed(2);
-  item2Total = (salaryWeekly + (commissionRate * item3Sold * item3Price)).toFixed(2);
-  item2Total = (salaryWeekly + (commissionRate * item4Sold * item4Price)).toFixed(2);
-  document.getElementById("item1Total").value = (gbp_value);
-  document.getElementById("item2Total").value = (can_value);
-  document.getElementById("item3Total").value = (eur_value);
-  document.getElementById("item4Total").value = (jpn_value);
+  item1Total = (salaryWeekly + (commissionRate * item1SoldB * item1Price)).toFixed(2);
+  item2Total = (salaryWeekly + (commissionRate * item2SoldB * item2Price)).toFixed(2);
+  item2Total = (salaryWeekly + (commissionRate * item3SoldB * item3Price)).toFixed(2);
+  item2Total = (salaryWeekly + (commissionRate * item4SoldB * item4Price)).toFixed(2);
+  document.getElementById("item1TotalB").value = (item1Total);
+  document.getElementById("item2TotalB").value = (item2Total);
+  document.getElementById("item3TotalB").value = (item3Total);
+  document.getElementById("item4TotalB").value = (item4Total);
 }

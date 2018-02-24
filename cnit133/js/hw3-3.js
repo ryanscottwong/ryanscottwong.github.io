27dcfec723773f90194a54d65123c5f4ec997ae7
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 14:45:55 -0800

    Update hw3-3.js

--- cnit133/js/hw3-3.js
+++ cnit133/js/hw3-3.js
@@ -1,16 +1,34 @@
-function celsius()
-{
-  rules: {
-    template: { required: true, number: true, min: 0}
-  },
-  messages: {
-    temp: "Please input a numeric value"
-  },
-  errorPlacement: function(error, element) {
-  if ( element.is(":text")) {
-    error.appendTo( element.parent());
-  }
-  else {
-    error.insertAfter(element);
+$(document).ready(function(){
+
+  /* JQuery for validation displays error message if form input is not a number */
+  $("#myform").validate({
+    // Rules for each input item
+    rules: {
+      acr: { required: true, number: true, min: 0 }
+    },
+    messages: {
+      acr: "Please input a numeric value"
+    },
+    errorPlacement: function(error, element) {
+      if ( element.is(":text")) {
+        error.appendTo( element.parent());
+      }
+      else {
+        error.insertAfter(element);
+      }
+    }
+  });	// end validation
+}); // document ready
+
+function celsius() {
+  if ($("#myform").valid()) {
+    // define variables
+    var tempC, tempF;
+    // get input number value from form using getElementById
+    tempC = document.getElementById("temp").value;
+    // convert temperature to Fahrenheit
+    tempF = parseInt((9 / 5) * (tempB - 32));
+    // display Fahrenheit temperature
+    document.forms["myform"].elements["conversion"].value = ("Temperature in Fahrenheit is " + tempF);
   }
 }

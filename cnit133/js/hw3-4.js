b03170886a940c842208f35a9e175bfe8cb27bf1
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 18:23:46 -0800

    Update hw3-4.js

--- cnit133/js/hw3-4.js
+++ cnit133/js/hw3-4.js
@@ -1,7 +1,8 @@
-function question() {
+window.onload = function question() {
   /* Generate two random integers between 0 and 9 */
   intA = Math.floor(Math.random() * 10);
   intB = Math.floor(Math.random() * 10);
+  answer = intA * intB;
   /* Display question */
   var element = document.getElementById("question");
   element.innerHTML = "What is " + intA + " times " + intB + "?";

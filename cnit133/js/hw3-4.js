ccf9c6e8540eb31b872b24a274ec6ed844f4d1a3
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 18:09:52 -0800

    Update hw3-4.js

--- cnit133/js/hw3-4.js
+++ cnit133/js/hw3-4.js
@@ -1,4 +1,4 @@
-function multiplication() {
+function question() {
   /* Generate two random integers between 0 and 9 */
   intA = Math.floor(Math.random() * 10);
   intB = Math.floor(Math.random() * 10);

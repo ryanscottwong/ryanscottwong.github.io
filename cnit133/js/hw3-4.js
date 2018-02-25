aa415756cebb59933ac586538b783db4f8d3fdd9
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 18:11:29 -0800

    Update hw3-4.js

--- cnit133/js/hw3-4.js
+++ cnit133/js/hw3-4.js
@@ -3,6 +3,6 @@ function question() {
   intA = Math.floor(Math.random() * 10);
   intB = Math.floor(Math.random() * 10);
   /* Display question */
-  var question = document.getElementById("question");
-  question.innerHTML = ("What is " + intA + " times " + intB + "?");
+  var element = document.getElementById("question");
+  element.innerHTML = ("What is " + intA + " times " + intB + "?");
 }

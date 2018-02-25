979ae02649a8e44ee02278c1e576bbca9efbfb6c
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-24 18:51:34 -0800

    Update hw3-4.js

--- cnit133/js/hw3-4.js
+++ cnit133/js/hw3-4.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-  /* Generate two random integers between 0 and 9 */
-  intA = Math.floor(Math.random() * 10);
-  intB = Math.floor(Math.random() * 10);
-  answer = intA * intB;
-  /* Display question */
-  var element = document.getElementById("question");
-  element.innerHTML = "What is " + intA + " times " + intB + "?";
+  $("#question").text("Hello world!");
 });

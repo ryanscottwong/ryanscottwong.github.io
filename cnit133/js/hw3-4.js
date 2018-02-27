851e4ac57f4240d0025662e9da328e1d93b1ab05
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-26 20:09:04 -0800

    Update hw3-4.js

--- cnit133/js/hw3-4.js
+++ cnit133/js/hw3-4.js
@@ -1,8 +1,23 @@
 $(function () {
-  /* Generate two random integers between 0 and 9 */
+  // Generate two random integers between 0 and 9
   intA = Math.floor(Math.random() * 10);
   intB = Math.floor(Math.random() * 10);
   answer = intA * intB;
-  /* Display question */
+  // Display question
   $("#question").text("How much is " + intA + " times " + intB + "?");
-});
+  return answer;
+}); // end document ready
+
+function getAnswer(answer) {
+  // Get student's answer from form using getElementById
+  studentAnswer = document.getElementById("answer").value;
+  // Compare student's answer to answer
+  if (studentAnswer != answer) {
+    document.getElementById("answer").value = "";
+    document.getElementById("message").value = "No. Please try again.";
+  }
+  else {
+    document.getElementById("answer").value = "";
+    document.getElementById("message").value = "Very good!";
+  }
+}

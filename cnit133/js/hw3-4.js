8ba805a035d850df465ea44fafe8718389bd4fe3
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-26 20:38:42 -0800

    Update hw3-4.js

--- cnit133/js/hw3-4.js
+++ cnit133/js/hw3-4.js
@@ -11,5 +11,5 @@ function getAnswer() {
   // Get student's answer from form using getElementById
   studentAnswer = document.forms["myform"].elements["answer"].value;
   // Compare student's answer to answer
-  document.forms["myform"].elements["message"].value = ("Your answer is " + studentAnswer);
+  document.forms["myform"].elements["message"].value = ("Your answer is ");
 }

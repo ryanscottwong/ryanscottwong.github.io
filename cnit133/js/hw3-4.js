13d5aed2a0629456fa7273d866602b834a89d893
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-26 22:24:40 -0800

    Update hw3-4.js

--- cnit133/js/hw3-4.js
+++ cnit133/js/hw3-4.js
@@ -1,4 +1,6 @@
-$(function () {
+var answer = -1;
+
+$(function (answer) {
   // Generate two random integers between 0 and 9
   intA = Math.floor(Math.random() * 10);
   intB = Math.floor(Math.random() * 10);

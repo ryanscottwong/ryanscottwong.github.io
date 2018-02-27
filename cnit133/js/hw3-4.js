0acff3dec9f21e0ed7b14f0f965ca05cd7c1edb9
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-02-26 23:17:24 -0800

    Update hw3-4.js

--- cnit133/js/hw3-4.js
+++ cnit133/js/hw3-4.js
@@ -23,7 +23,7 @@ function getAnswer() {
   else {
     document.getElementById("message").style.color = "green";
     document.getElementById("message").innerHTML = ("Very good!");
-    setTimeout(confirmF, 3000);
+    setTimeout(confirmF, 2000);
   }
 }
 

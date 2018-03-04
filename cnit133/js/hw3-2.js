47a015d595d627eea2cc8503e76ea74686ecb8fc
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-03-04 10:17:05 -0800

    Update hw3-2.js

--- cnit133/js/hw3-2.js
+++ cnit133/js/hw3-2.js
@@ -1,30 +1,59 @@
+$(document).ready(function(){
+
+  /* JQuery for validation displays error message if form input is a negative number */
+  $("#salespersonform").validate({
+    // Rules for each input item
+    rules: {
+      item1Sold: { required: true, number: true, min: 0, max: 100},
+      item2Sold: { required: true, number: true, min: 0, max: 100},
+      item3Sold: { required: true, number: true, min: 0, max: 100},
+      item4Sold: { required: true, number: true, min: 0, max: 100}
+    },
+    messages: {
+       item1Sold: "Please enter a number greater than zero",
+       item2Sold: "Please enter a number greater than zero",
+       item3Sold: "Please enter a number greater than zero",
+       item4Sold: "Please enter a number greater than zero"
+    },
+    errorPlacement: function(error, element) {
+      if ( element.is(":text")) {
+        error.appendTo( element.parent());
+      }
+      else {
+        error.insertAfter(element);
+      }
+    }
+  });	// end validation
+}); // document ready
+
 /* This script calculates how much the salesperson sold, how much they earned, and then outputs the results */
-function total()
-{
-  salaryWeekly = 200.00;
-  commissionRate = 0.09;
-  item1Price = 239.99;
-  item2Price = 129.75;
-  item3Price = 99.95;
-  item4Price = 350.89;
-  item1SoldB = document.getElementById("item1Sold").value;
-  item2SoldB = document.getElementById("item2Sold").value;
-  item3SoldB = document.getElementById("item3Sold").value;
-  item4SoldB = document.getElementById("item4Sold").value;
-  item1Total = (item1SoldB * item1Price).toFixed(2);
-  item2Total = (item2SoldB * item2Price).toFixed(2);
-  item3Total = (item3SoldB * item3Price).toFixed(2);
-  item4Total = (item4SoldB * item4Price).toFixed(2);
-  amountSold = (item1SoldB * item1Price + item2SoldB * item2Price + item3SoldB * item3Price + item4SoldB * item4Price).toFixed(2);
-  weeklyEarnings = (salaryWeekly + (commissionRate * amountSold)).toFixed(2);
-  document.getElementById("item1SoldB").value = (item1SoldB);
-  document.getElementById("item2SoldB").value = (item2SoldB);
-  document.getElementById("item3SoldB").value = (item3SoldB);
-  document.getElementById("item4SoldB").value = (item4SoldB);
-  document.getElementById("item1TotalB").value = (item1Total);
-  document.getElementById("item2TotalB").value = (item2Total);
-  document.getElementById("item3TotalB").value = (item3Total);
-  document.getElementById("item4TotalB").value = (item4Total);
-  document.getElementById("amountSoldB").value = (amountSold);
-  document.getElementById("weeklyEarningsB").value = (weeklyEarnings);
+function total() {
+  if ($("#salespersonform").valid()) {
+    salaryWeekly = 200.00;
+    commissionRate = 0.09;
+    item1Price = 239.99;
+    item2Price = 129.75;
+    item3Price = 99.95;
+    item4Price = 350.89;
+    item1SoldB = document.getElementById("item1Sold").value;
+    item2SoldB = document.getElementById("item2Sold").value;
+    item3SoldB = document.getElementById("item3Sold").value;
+    item4SoldB = document.getElementById("item4Sold").value;
+    item1Total = (item1SoldB * item1Price).toFixed(2);
+    item2Total = (item2SoldB * item2Price).toFixed(2);
+    item3Total = (item3SoldB * item3Price).toFixed(2);
+    item4Total = (item4SoldB * item4Price).toFixed(2);
+    amountSold = (item1SoldB * item1Price + item2SoldB * item2Price + item3SoldB * item3Price + item4SoldB * item4Price).toFixed(2);
+    weeklyEarnings = (salaryWeekly + (commissionRate * amountSold)).toFixed(2);
+    document.getElementById("item1SoldB").value = (item1SoldB);
+    document.getElementById("item2SoldB").value = (item2SoldB);
+    document.getElementById("item3SoldB").value = (item3SoldB);
+    document.getElementById("item4SoldB").value = (item4SoldB);
+    document.getElementById("item1TotalB").value = (item1Total);
+    document.getElementById("item2TotalB").value = (item2Total);
+    document.getElementById("item3TotalB").value = (item3Total);
+    document.getElementById("item4TotalB").value = (item4Total);
+    document.getElementById("amountSoldB").value = (amountSold);
+    document.getElementById("weeklyEarningsB").value = (weeklyEarnings);
+  }
 }

8ce86e28bfb7638ea7d68f7be4ab9d3f0c4f7eb8
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-03-18 11:47:14 -0700

    Update hw4-1.js

--- cnit133/js/hw4-1.js
+++ cnit133/js/hw4-1.js
@@ -1,3 +1,38 @@
 $(function() {
-   $( "#draggable" ).draggable();
+
+  /* This script calculates the product and sum of every third integer
+  from 5 to 20 inclusive and then displays the results */
+  var sum = 0;
+  var product = 1;
+  for ( var number = 5; number <= 20; number += 3 )
+  {
+     sum += number;
+     product *= number;
+  }
+  productComma = product.toLocaleString(undefined, {maximumFractionDigits: 0});
+  document.getElementById("third").innerHTML =
+    ("The sum of every third integer " +
+      "from 5 to 20 inclusive is " + sum +
+      "<br>The product of every third integer " +
+      "from 5 to 20 inclusive is " +  productComma);
+
+  /* This script calculates the product and sum of every fourth integer from 3 to 31 inclusive and then displays the results */
+  var number = 3;
+  var sum = 0;
+  var product = 1;
+  while ( number >= 3 && number <=31 ) {
+     sum += number;
+     product *= number;
+     number += 4;
+  }
+  productComma = product.toLocaleString(undefined, {maximumFractionDigits: 0});
+  document.getElementById("fourth").innerHTML =
+    ("The sum of every fourth integer " +
+      "from 3 to 31 inclusive is " + sum +
+      "<br>The product of every fourth integer " +
+      "from 3 to 31 inclusive is " +  productComma);
+
+  // draggable div
+  $( "#draggable" ).draggable();
+
 });

e10e16857dd18a57c2b00ce7d55789de2d37fb14
ryanscottwong <35509210+ryanscottwong@users.noreply.github.com>
2018-04-28 15:08:01 -0700

    Create hw7.js

---
+++ cnit133/js/hw7.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.answer').hide();
+
+    // Attach a click handler to the h2 elements.
+    $(".main p").click(function () {
+        // Save the h2 element that was clicked
+        // as well as the following answer element.
+        var h2 = $(this);
+        var answer = h2.next(".answer");
+
+        if (answer.css("display") == "none") {
+            // If the answer is currently not displayed,
+            // display it with a sliding down motion,
+            // and when done, add the "close" class to the h2 element.
+            answer.slideDown(function () {
+                h2.addClass("close");
+            });
+        }
+        else {
+            // Otherwise, fade the answer out, and when done,
+            // remove the "close" class from the h2 element.
+            answer.slideUp(function () {
+                h2.removeClass("close");
+            });
+        }
+    });
+}); // end ready
